119f9e9ea31b1d59601c28ed8d591ec782abfaf4
praha-poseidon <xiyangyulei@sina.cn>
2026-08-01 12:36:11 +0800

    Add sendNodeEvent for NodeChangeEvent handling

--- code-graph-core/src/main/java/com/poseidon/codegraph/engine/domain/context/GraphSender.java
+++ code-graph-core/src/main/java/com/poseidon/codegraph/engine/domain/context/GraphSender.java
@@ -1,6 +1,7 @@
 package com.poseidon.codegraph.engine.domain.context;
 
 import com.poseidon.codegraph.model.event.CodeChangeEvent;
+import com.poseidon.codegraph.model.event.NodeChangeEvent;
 import lombok.Data;
 import java.util.function.Consumer;
 
@@ -10,10 +11,14 @@ import java.util.function.Consumer;
  */
 @Data
 public class GraphSender {
-    
+
     /**
      * 发送代码变更事件
      */
     private Consumer<CodeChangeEvent> sendEvent;
-}
 
+    /**
+     * 发送节点级变更事件（增/删/改）
+     */
+    private Consumer<NodeChangeEvent> sendNodeEvent;
+}
